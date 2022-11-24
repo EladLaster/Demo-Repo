e93372ce4cb285c1e45d0e799d65f53c559ff542
EladLaster <118683420+EladLaster@users.noreply.github.com>
2022-11-24 14:51:33 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -4,6 +4,8 @@
 
 
 int isPrime(int x){
+    if(x==0)
+        return 0;
     for(int i=2;i<=sqrt(x);i++){
         if((x%i)==0){
             return 0;
@@ -12,18 +14,24 @@ int isPrime(int x){
         return 1;
 }
 int isStrong(int x){
+    if(x==0)
+        return 0;
     int temp=0;
     int sum=0;
-    while(x>0){
-        temp=x%10;
-        sum=sum+factorial(temp);
-        x=x/10;
+    int num=x;
+    while(num>0){
+        temp=num%10;
+        if(temp==0)
+            sum=sum+1;
+        else
+            sum=sum+factorial(temp);
+        num=num/10;
     }
     if(x==sum)
         return 1;
 
-        return 0;
-        }
+    return 0;
+}
 int factorial(int x){
     int fact=x;
     for(int i=x-1;i>0;i--){
